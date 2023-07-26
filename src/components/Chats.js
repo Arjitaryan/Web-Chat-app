88aef04d09a38a4b0f569827dfd0b200e2d5cb6d
Arjit <arjitaryan3611@gmail.com>
2023-07-26 16:56:28 +0530

    Implemented Search user and made chat database

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,7 +1,12 @@
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useContext, useEffect, useState } from "react";
+import { db } from "../firebase";
+import { AuthContext } from "../context/AuthContext";
 
 function Chats(){
+    
     return (
+
         <div className="chats">
             <div className="userChat">
                 <img src="https://plus.unsplash.com/premium_photo-1675130119373-61ada6685d63?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHBlcnNvbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=500&q=60" alt=""/>
